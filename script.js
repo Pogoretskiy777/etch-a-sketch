4570e0813646f2eed90a3b9d911f1f28436ee3c9
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2023-05-27 16:35:52 -0400

    Added basic foundation of the program

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-let container = document.querySelector("container");
+let container = document.querySelector(".container");
+let cell = document.createElement("div");
+cell.classList.add("cell");
+
+for (i = 0; i < 256; i++) {
+  container.appendChild(cell.cloneNode(true));
+}
