b6d80987986b29cef94a9667cd74172eaa680b90
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2023-05-27 16:54:36 -0400

    Revised functionality

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ cell.classList.add("cell");
 for (i = 0; i < 256; i++) {
   container.appendChild(cell.cloneNode(true));
 }
+
+let grid = document.querySelectorAll(".cell");
+grid.forEach((square) => {
+  if (square.getPropertyValue("background-color" === "white"))
+    square.setAttribute("background-color", "white");
+});
